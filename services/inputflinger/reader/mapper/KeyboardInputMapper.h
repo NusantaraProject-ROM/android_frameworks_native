da18ba6ca0a5107f2ba4a54b33bb98cf68451b5d
Willi Ye <williye97@gmail.com>
2016-03-15 21:13:47 +0100

    Navbar: Swap 'Back' and 'Recents' hw keys
    Lucchetto edits:
    - Adapt to Android 10
    Genkzsz:
    - Adapt to Android 11 & 12
    
    Change-Id: I0f0a8c5921e1b4cefaeaad3a0b2b638c4d9daebf
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- services/inputflinger/reader/mapper/KeyboardInputMapper.h
+++ services/inputflinger/reader/mapper/KeyboardInputMapper.h
@@ -55,6 +55,7 @@ private:
 
     uint32_t mSource;
     int32_t mKeyboardType;
+    bool mSwapKeys; // swap back with recents button
 
     std::vector<KeyDown> mKeyDowns; // keys that are down
     int32_t mMetaState;
@@ -87,6 +88,7 @@ private:
     bool isMediaKey(int32_t keyCode);
 
     void processKey(nsecs_t when, nsecs_t readTime, bool down, int32_t scanCode, int32_t usageCode);
+    int getAdjustedKeyCode(int keyCode);
 
     bool updateMetaStateIfNeeded(int32_t keyCode, bool down);
 
