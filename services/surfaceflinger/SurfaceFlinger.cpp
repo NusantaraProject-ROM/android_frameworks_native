bad9084a220c4d7f269eaa3baa4d037a5c8a5e0f
jianying duan <jianying.duan@mediatek.com>
2020-11-24 16:49:57 +0800

    Fix syncpoint frameNumber not align issue
    syncpoint framenumber not align due to 3 points:
    1. expect present time not align between handleTRansaction and
    handlePageFlip
    2. fence is not signal when notifyAvailableFrames call
    getHeadFrameNumber,but fence signaled when allTransactionSignaled call
    getHeadFrameNumber
    3. mQueueItems add more members after handleTransaction,
    
    above 3 points will leads to that notifyAvailableFrames get
    headFrameNumber N but allTransactionSignaled get headFrameNumber N+1,
    then it will latchbuffer fail.
    
    solution:
    1. delete useless code which update expect present time in
    setTransactionState.
    2. add judge condition of latchUnsignaledBuffers
    3. record a frameNumber when notifyAvailableFrames
    and use it when getFrameNumeber and updateTeximage.
    
    Bug: 174211100
    Change-Id: Icbf440eb20ddc0ba42a6e0257ec6f686bc05d9c8
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2000,6 +2000,7 @@ void SurfaceFlinger::onMessageInvalidate(nsecs_t expectedVSyncTime) {
 
         refreshNeeded = handleMessageTransaction();
         refreshNeeded |= handleMessageInvalidate();
+        clearCurrentStateLayerNotifiedFrameNumber();
         if (mTracingEnabled) {
             mAddCompositionStateToTrace =
                     mTracing.flagIsSetLocked(SurfaceTracing::TRACE_COMPOSITION);
@@ -3072,6 +3073,7 @@ void SurfaceFlinger::commitTransactionLocked() {
             // Ensure any buffers set to display on any children are released.
             if (l->isRemovedFromCurrentState()) {
                 l->latchAndReleaseBuffer();
+                l->clearNotifiedFrameNumber();
             }
 
             // If the layer has been removed and has no parent, then it will not be reachable
@@ -3408,10 +3410,6 @@ void SurfaceFlinger::setTransactionState(
     }
 
     const bool pendingTransactions = itr != mTransactionQueues.end();
-    // Expected present time is computed and cached on invalidate, so it may be stale.
-    if (!pendingTransactions) {
-        mExpectedPresentTime = calculateExpectedPresentTime(systemTime());
-    }
 
     if (pendingTransactions || !transactionIsReadyToBeApplied(desiredPresentTime, states)) {
         mTransactionQueues[applyToken].emplace(states, displays, flags, desiredPresentTime,
@@ -6405,6 +6403,13 @@ void SurfaceFlinger::enableRefreshRateOverlay(bool enable) {
             mRefreshRateOverlay->changeRefreshRate(mRefreshRateConfigs->getCurrentRefreshRate());
         }
     }));
+}
+
+void SurfaceFlinger::clearCurrentStateLayerNotifiedFrameNumber() {
+    Mutex::Autolock _l(mStateLock);
+    mCurrentState.traverseInZOrder([](Layer* layer) {
+        layer->clearNotifiedFrameNumber();
+    });
 }
 
 } // namespace android
